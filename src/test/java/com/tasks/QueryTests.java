b4a885fae70cc07bd45bd2dc28dcc6c73da71e63
lipj <Lipj_java@126.com>
2019-08-25 18:22:15 +0800

    no message

--- src/test/java/com/tasks/QueryTests.java
+++ src/test/java/com/tasks/QueryTests.java
@@ -12,7 +12,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -21,16 +23,18 @@ public class QueryTests {
     private KieSession kieSession;
 
     @Test
-    public void queryDataTest(){
+    public void queryDataVIPUserTest(){
         List<VIPUser> vipUserList=new ArrayList<>();
         VIPUser vipUser1=new VIPUser();
         vipUser1.setName("张三");
         vipUser1.setVipNo("110");
         vipUserList.add(vipUser1);
+
         VIPUser vipUser2=new VIPUser();
         vipUser2.setName("刘备");
         vipUser2.setVipNo("111");
         vipUserList.add(vipUser2);
+
         VIPUser vipUser3=new VIPUser();
         vipUser3.setName("李五");
         vipUser3.setVipNo("112");
@@ -57,7 +61,57 @@ public class QueryTests {
         kieSession.insert(user1);
 
         kieSession.insert(user2);
-        kieSession.fireAllRules(new RuleNameEqualsAgendaFilter("queryTest"));
+//        kieSession.fireAllRules(new RuleNameEqualsAgendaFilter("queryTest"));
+        kieSession.fireAllRules(new RuleNameEqualsAgendaFilter("queryTest3"));
+        kieSession.dispose();
+    }
+
+
+    @Test
+    public void queryDataMapTest(){
+        List<Map<String,Object>> vipUserList=new ArrayList<>();
+
+        Map map1=new HashMap();
+        map1.put("name","张三");
+        map1.put("vipNo","110");
+        vipUserList.add(map1);
+
+        Map map2=new HashMap();
+        map2.put("name","刘备");
+        map2.put("vipNo","111");
+        vipUserList.add(map2);
+
+        Map map3=new HashMap();
+        map3.put("name","李五");
+        map3.put("vipNo","112");
+        vipUserList.add(map3);
+
+        Map map4=new HashMap();
+        map4.put("name","刘备");
+        map4.put("vipNo","3");
+        vipUserList.add(map4);
+
+
+        Map map5=new HashMap();
+        map5.put("name","关羽");
+        map5.put("vipNo","123456");
+        vipUserList.add(map5);
+
+
+        //设置全局变量
+        kieSession.setGlobal("vipUserList",vipUserList);
+        User user1=new User();
+        user1.setName("张三");
+        user1.setAge(12);
+
+        User user2=new User();
+        user2.setName("刘备");
+        user2.setAge(12);
+
+        kieSession.insert(user1);
+
+        kieSession.insert(user2);
+        kieSession.fireAllRules(new RuleNameEqualsAgendaFilter("queryMapTest"));
         kieSession.dispose();
     }
 }
